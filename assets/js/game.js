35ec3c13a08dcc93e67d94ce0bfa2109a70fda3c
Garrett Cox <gacx89@gmail.com>
2020-10-05 18:58:44 -0500

    added array for robot names and for loop to fight each robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+for (var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+var fight = function (enemyName) {
   window.alert("Welcome to Robot Gladiators!");
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -44,7 +47,7 @@ var fight = function () {
     // confirm user wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit");
 
-    if (confirmSkip){
+    if (confirmSkip) {
       window.alert(playerName + " has chosen to skip this fight! Goodbye!");
       // subtract money from playerMoney for skipping
       playerMoney = playerMoney - 2;
@@ -60,4 +63,6 @@ var fight = function () {
 
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
